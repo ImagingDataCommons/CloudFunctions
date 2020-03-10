e0bbcdcde8422fadeeb96a3bad81f8c78a2e8196
wlongabaugh <wlongabaugh@systemsbiology.org>
2020-03-09 18:06:51 -0700

    move import

--- python/LogPubSubMessage.py
+++ python/LogPubSubMessage.py
@@ -1,7 +1,8 @@
 
+import base64
 
 def sink_pubsub_to_logs(event, context):
     """Logs a pubsub message"""
-    import base64
+
     data = base64.b64decode(event['data']).decode('utf-8') if 'data' in event else "none"
     print('PubSub messageId {} published at {} with data: {}'.format(context.event_id, context.timestamp, data))
